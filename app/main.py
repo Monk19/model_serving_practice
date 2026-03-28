432a093525d8955276c098ae62de91ed9a77712c
Ravi Sanker <ravigadu.19@gmail.com>
2026-03-28 14:08:01 +0530

    Added Health check and ready end points

--- app/main.py
+++ app/main.py
@@ -11,6 +11,16 @@ model = joblib.load("app/model.pkl")
 def read_root():
     return {"message": "House Price Prediction API is Up!"}
 
+@app.get("/health")
+async def health_check():
+    return {"status": "healthy", "version": "1.0.0"}
+
+@app.get("/ready")
+async def readiness_check():
+    if model_is_loaded: # Check if your .pkl or .h5 file is ready
+        return {"status": "ready"}
+    return JSONResponse(status_code=503, content={"status": "loading_model"})
+
 @app.post("/predict")
 def predict(data: list):
     # Data should be a list of 8 features (MedInc, HouseAge, etc.)
